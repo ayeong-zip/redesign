9739e7c6e144f9275437910b6af704853c5531d9
kindtiger <seonbeom2@gmail.com>
2025-02-26 19:55:39 +0900

    2차 컨펌

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import "../lib/smooth";
 import "./styles/style.css";
+import {Swiper} from 'Swiper';
+// import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+console.log( Swiper );
 
 // import { gsap } from "gsap";
 // import { Draggable } from "gsap/Draggable";
@@ -12,3 +19,14 @@ import "./styles/style.css";
 // 1. 터미널 창에서 npm i 실행한다.
 // 3. npm run dev로 사이트를 연다.
 // 5. 코딩 시작.
+
+
+const swiper = new Swiper('.bg-img', {
+
+  loop: true,
+    // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+});
