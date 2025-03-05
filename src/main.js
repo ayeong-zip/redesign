519cd4955828c7c6cd4ca28e70ade4bada969c6f
kindtiger <seonbeom2@gmail.com>
2025-03-05 21:28:05 +0900

    4차 컨펌

--- src/main.js
+++ src/main.js
@@ -1,25 +1,27 @@
 import "../lib/smooth";
 import "./styles/style.css";
-import {Swiper} from 'Swiper';
+import { markers } from "../lib/smooth";
 // import { Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 
-console.log( Swiper );
-
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
 // import { Draggable } from "gsap/Draggable";
 // import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// gsap.registerPlugin(ScrollTrigger,Draggable,MotionPathPlugin);
+gsap.registerPlugin(ScrollTrigger);
 
 // 1. git pull 입력
 // 1. 터미널 창에서 npm i 실행한다.
 // 3. npm run dev로 사이트를 연다.
 // 5. 코딩 시작.
 
+
+/* section01  */
 
 const swiper = new Swiper('.bg-img', {
 
@@ -29,4 +31,57 @@ const swiper = new Swiper('.bg-img', {
     el: '.swiper-pagination',
   },
 
+
 });
+
+
+
+
+/* section02 */
+
+ScrollTrigger.create({
+  trigger: '.section02',
+  start: 'top top',
+  end: 'bottom bottom',
+  animation: gsap.from('.shelving .text-top',{y:30,opacity:0}),
+  // pin: false,
+  // pinSpacing: false,
+  // markers: true,
+  // scrub: true,
+})
+
+
+ScrollTrigger.create({
+  trigger: '.section02',
+  start: 'top top',
+  end: 'bottom bottom',
+  animation: gsap.from('.Cabinets .text-top',{y:30,opacity:0}),
+  // pin: false,
+  // pinSpacing: false,
+  // markers: true,
+  // scrub: true,
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+markers()
+
+
+
+
+
+
+
+
